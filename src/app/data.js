5cf261b5e887023768cdab7952ddf1cf2b895c66
Nimish Vadgaonkar <116025943+Nimi2607@users.noreply.github.com>
2024-12-13 17:10:01 +0530

    Add files via upload

---
+++ src/app/data.js
@@ -0,0 +1,68 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Waste Management System",
+    description: "Empowering Urban Communities to Reduce Waste",
+    date: "2024-12-12",
+    demoLink: "https://waste-mgmt-sys.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Waste Management PowerBi Dashboard",
+    description: "Get realtime insights from the Waste Management System",
+    date: "2024-12-12",
+    demoLink: "https://somaiya0-my.sharepoint.com/:u:/g/personal/n_vadgaonkar_somaiya_edu/EajFz3FR6-xInT8YIzIhLgoBqYQLkdA7HJxXkhRi2SnxaQ?e=gNiEon",
+  },
+
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/Nimi2607",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/nimish-vadgaonkar-aaa637255/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "Instagram",
+    link: "https://www.instagram.com/nimishvadgaonkar/",
+    icon: "Instagram",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
